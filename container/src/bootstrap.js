36a07a04dd7fa646b9995ec2abe3fdc9b2abfe6b
harishkumarforever <harishking809@gmail.com>
2023-05-16 23:08:50 +0530

    new changes added

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,6 @@
 import 'products/ProductsIndex';
+// container
+// console.log('Container!');
 
-console.log('Container!');
+document.querySelector('#container').innerHTML = `<div> container from parents </div>`;
+
